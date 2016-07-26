b48c3fe06632be8324f4cd7e6ef42944b68c5a4f
Oamazing <2569202365@qq.com>
2016-07-26 17:00:23 +0800

    跨位置相加 2.一次同列多个运算

--- 1024/js/options.js
+++ 1024/js/options.js
@@ -5,9 +5,9 @@ new Vue({
         record:'4096',
         panel:[
             {value:2,class:'class2'},{value:'2',class:'class2'},{value:'',class:'class0'},{value:2,class:'class2'},            
-            {value:2,class:'class2'},{value:'',class:'class0'},{value:'8',class:'class8'},{value:'',class:'class0'},                        
+            {value:2,class:'class2'},{value:'8',class:'class8'},{value:'8',class:'class8'},{value:'',class:'class0'},                        
             {value:'',class:'class0'},{value:'8',class:'class8'},{value:'8',class:'class8'},{value:'',class:'class'},            
-            {value:8,class:'class8'},{value:'8',class:'class8'},{value:'2',class:'class2'},{value:'2',class:'class2'},            
+            {value:8,class:'class8'},{value:'',class:'class0'},{value:'2',class:'class2'},{value:'2',class:'class2'},            
             
             
         ],
@@ -20,7 +20,9 @@ new Vue({
             sPos : {},
             mPos : {},
             dire:'',
-        }
+            switch:false,
+        },
+        tipsShow:false
     },
     created:function(){
         this.li.height = document.querySelector('li').offsetWidth +'px';
@@ -52,13 +54,14 @@ new Vue({
                 this.touch.dire = 'L';
             }
             this.compute(this.touch.dire);
-            //console.log(this.touch.dire);
         },
         compute:function(dire){
             var This = this;
+            this.touch.switch = false;
             switch(dire){
                 case 'D':
-                    for(var i = 0;i < 11;i++){
+                    //计算
+                    for(var i = 0;i <= 11;i++){
                         if( Number(this.panel[i].value) != 0){
                             for(var x = 1;x < 4;x++){
                                 if(i+4*x < 16 && Number(this.panel[i].value) ==  Number(this.panel[i+4*x].value)){
@@ -68,6 +71,20 @@ new Vue({
                             }               
                         }
                     }
+                    //移动 
+                    for(var i = 12;i < 16;i++){
+                        for(var x = i;x >= i-8;x = x-4){
+                           if( Number(this.panel[x].value) == 0){
+                                for(var y = x; y >= i-12; y = y-4){
+                                    if(Number(this.panel[y].value) != 0){
+                                        This.move(x,y);
+                                        break;
+                                    }
+                                }
+                                
+                           } 
+                        }
+                    }
                     break;
                 case 'U':
                     for(var i = 15;i > 3;i--){
@@ -75,9 +92,21 @@ new Vue({
                             for(var x = 1;x < 4;x++){
                                 if(i-4*x >= 0 && Number(this.panel[i].value) ==  Number(this.panel[i-4*x].value)){
                                     This.add(i,i-4*x);
-                                    break;
+                                    break;  //相邻的元素计算后推出
                                 }
                             }                   
+                        }
+                    }
+                    for(var i = 0;i < 4;i++){
+                        for(var x = i;x <= i+8;x = x+4){
+                           if( Number(this.panel[x].value) == 0){
+                                for(var y = x; y <= i+12; y = y+4){
+                                    if(Number(this.panel[y].value) != 0){
+                                        This.move(x,y);
+                                        break;
+                                    }
+                                }                                
+                           } 
                         }
                     }
                     break;
@@ -89,28 +118,71 @@ new Vue({
                                     if(Number(This.panel[i].value) ==  Number(This.panel[x-1].value)){
                                         This.add(i,x-1);
                                         break;
+                                    }else{
+                                        break;
                                     }
                                 }                   
                             }                        
                         }
                     }
+                    //移动
+                    for(var i = 0;i < 13;i = i+4){
+                        for(var x = i;x <= i+2;x++){
+                           if( Number(this.panel[x].value) == 0){
+                                for(var y = x; y <= i+3; y++){
+                                    if(Number(this.panel[y].value) != 0){
+                                        This.move(x,y);
+                                        break;
+                                    }
+                                }                                
+                           } 
+                        }
+                    }
                     break;
                 case 'R':
-                    for(var r = 0;r < 3;r++){
-                        console.log(r)
-                        for(var i = r;i <= r+12;i = i+4){
+                    for(var r = 0;r < 3;r++){   //前三列
+                        for(var i = r;i <= r+12;i = i+4){   //四行
                             if( Number(this.panel[i].value) != 0){
-                                for(var x = i;x < i + (3-r) ;x++){
+                                for(var x = i;x < i + (3-r) ;x++){  //每一行的比较相同则 计算，是不同列 所以比较后都要退出换i值
                                     if(Number(this.panel[i].value) ==  Number(this.panel[x+1].value)){
                                         This.add(i,x+1);
+                                        break;
+                                    }else{
                                         break;
                                     }
                                 }                   
                             }
                         }
                     }
+                    //移动
+                    for(var i = 3;i < 16;i = i+4){  // 从右侧的3,7,11,15分四列开始检测
+                        for(var x = i;x >= i-2;x--){    //每一列的遍历
+                           if( Number(this.panel[x].value) == 0){   //如果当前元素为空 则从前面找不为空的元素置换位置
+                                for(var y = x; y >= i-3; y--){
+                                    if(Number(this.panel[y].value) != 0){
+                                        This.move(x,y);
+                                        break;
+                                    }
+                                }                                
+                           } 
+                        }
+                    }
                     break;
             }
+            var arr = [];   //存储value为空的元素index 
+            this.panel.forEach(function(ele,index){
+                if(Number(ele.value) === 0){
+                    arr.push(index);
+                }
+            });
+            if(arr.length == 0){
+                this.tipsShow = true;
+            }else if(this.touch.switch){    //如果有像加的计算
+                var newIndex = Math.round(Math.random()*arr.length);    //新元素的位置
+                this.panel[newIndex].value = 2;
+                this.panel[newIndex].class = 'class2';
+            }
+            
         },
         add:function(oldindex,newindex){
             var This = this;
@@ -118,6 +190,14 @@ new Vue({
             This.panel[oldindex].value = '';
             This.panel[oldindex].class = 'class';
             This.panel[newindex].class = 'class' + This.panel[newindex].value;
+            this.touch.switch = true;
         },
+        move:function(index1,index2){   //index1目标位置 index2被移动的位置
+            this.panel[index1].value = this.panel[index2].value;
+            this.panel[index1].class = this.panel[index2].class;
+            this.panel[index2].value = '';
+            this.panel[index2].class = 'class0';
+            this.touch.switch = true;
+        }
     }
 })
