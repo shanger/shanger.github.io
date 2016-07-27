ec88a535306cc69dbaeaeb3da9a6bdf86af0866b
Oamazing <2569202365@qq.com>
2016-07-27 18:08:36 +0800

    解决了跨位置相加，但一次做多个运算的问题还在

--- 1024/js/options.js
+++ 1024/js/options.js
@@ -1,13 +1,13 @@
-new Vue({
+var pannel = new Vue({
     el:'#panel',
     data:{
-        score:'2048',
-        record:'4096',
+        score:0,
+        record:localStorage.getItem('record')?localStorage.getItem('record'):0,
         panel:[
-            {value:2,class:'class2'},{value:'2',class:'class2'},{value:'',class:'class0'},{value:2,class:'class2'},            
-            {value:2,class:'class2'},{value:'8',class:'class8'},{value:'8',class:'class8'},{value:'',class:'class0'},                        
-            {value:'',class:'class0'},{value:'8',class:'class8'},{value:'8',class:'class8'},{value:'',class:'class'},            
-            {value:8,class:'class8'},{value:'',class:'class0'},{value:'2',class:'class2'},{value:'2',class:'class2'},            
+            {value:2,class:'class2'},{value:'',class:'class'},{value:2,class:'class2'},{value:2,class:'class2'},            
+            {value:2,class:'class2'},{value:'',class:'class'},{value:'',class:'class'},{value:'',class:'class0'},                        
+            {value:'',class:'class0'},{value:'',class:'class'},{value:'',class:'class'},{value:'',class:'class'},            
+            {value:'',class:'class'},{value:'',class:'class0'},{value:'',class:'class'},{value:'',class:'class'},            
             
             
         ],
@@ -27,6 +27,7 @@ new Vue({
     created:function(){
         this.li.height = document.querySelector('li').offsetWidth +'px';
         this.li.marginTop = Math.floor(document.querySelector('li').offsetWidth/24) + 'px';
+        this.reset();
     },
     methods:{
         touchstart:function($event){
@@ -60,15 +61,26 @@ new Vue({
             this.touch.switch = false;
             switch(dire){
                 case 'D':
-                    //计算
-                    for(var i = 0;i <= 11;i++){
+                    //计算                   
+                    for(var i = 15;i > 3;i--){
                         if( Number(this.panel[i].value) != 0){
-                            for(var x = 1;x < 4;x++){
-                                if(i+4*x < 16 && Number(this.panel[i].value) ==  Number(this.panel[i+4*x].value)){
-                                    This.add(i,i+4*x);
-                                    break;
+                           for(var x = 1;x < 4;x++){
+                                if( i-4*x >= 0 && Number(this.panel[i].value) ==  Number(this.panel[i-4*x].value)){
+                                    if(x > 2 && Number(this.panel[i-8].value) == 0){    //跨非空元素不能相加
+                                        This.add(i,i-4*x); //相邻的元素计算后推出
+                                        break;
+                                    }else if(x > 1 && Number(this.panel[i-4].value) == 0 && Number(this.panel[i-8].value) == 0){    //跨非空元素不能相加
+                                        This.add(i,i-4*x); //相邻的元素计算后推出
+                                        break; 
+                                    }else if(x == 1){
+                                        This.add(i,i-4*x); //相邻的元素计算后推出
+                                        break;
+                                    }
+                                    
+                                   
                                 }
-                            }               
+                                
+                            }                  
                         }
                     }
                     //移动 
@@ -87,14 +99,22 @@ new Vue({
                     }
                     break;
                 case 'U':
-                    for(var i = 15;i > 3;i--){
+                    for(var i = 0;i <= 11;i++){
                         if( Number(this.panel[i].value) != 0){
                             for(var x = 1;x < 4;x++){
-                                if(i-4*x >= 0 && Number(this.panel[i].value) ==  Number(this.panel[i-4*x].value)){
-                                    This.add(i,i-4*x);
-                                    break;  //相邻的元素计算后推出
-                                }
-                            }                   
+                                if(i+4*x < 16 && Number(this.panel[i].value) ==  Number(this.panel[i+4*x].value)){
+                                    if(x > 2 && Number(this.panel[i+8].value) == 0&& Number(this.panel[i+4].value) == 0){    //跨非空元素不能相加
+                                        This.add(i,i+4*x); //相邻的元素计算后推出
+                                        break;
+                                    }else if(x > 1  && Number(this.panel[i+4].value) == 0){    //跨非空元素不能相加
+                                        This.add(i,i+4*x); //相邻的元素计算后推出
+                                        break; 
+                                    }else if(x == 1){
+                                        This.add(i,i+4*x); //相邻的元素计算后推出
+                                        break;
+                                    }                                  
+                                }                                
+                            }               
                         }
                     }
                     for(var i = 0;i < 4;i++){
@@ -111,15 +131,22 @@ new Vue({
                     }
                     break;
                 case 'L':
+                    //计算
                     for(var l = 3;l > 0;l--){ //从第四列依次和前三列相加 一次相加之后推出循环不再相加
                         for(var i = l;i <= l+12; i=i+4){
                             if( Number(This.panel[i].value) != 0){
                                 for(var x = i;x > i-l;x--){
                                     if(Number(This.panel[i].value) ==  Number(This.panel[x-1].value)){
-                                        This.add(i,x-1);
-                                        break;
-                                    }else{
-                                        break;
+                                        if(i-x > 2 && Number(this.panel[i-1].value) == 0 && Number(this.panel[i-2].value) == 0){    //跨非空元素不能相加
+                                            This.add(i,x-1); //相邻的元素计算后推出
+                                            break;
+                                        }else if(i-x > 1 && Number(this.panel[i-1].value) ){    //跨非空元素不能相加
+                                            This.add(i,x-1); //相邻的元素计算后推出
+                                            break; 
+                                        }else if(i-x <= 1){
+                                            This.add(i,x-1); //相邻的元素计算后推出
+                                            break;
+                                        }
                                     }
                                 }                   
                             }                        
@@ -140,15 +167,23 @@ new Vue({
                     }
                     break;
                 case 'R':
+                    //计算
                     for(var r = 0;r < 3;r++){   //前三列
                         for(var i = r;i <= r+12;i = i+4){   //四行
                             if( Number(this.panel[i].value) != 0){
                                 for(var x = i;x < i + (3-r) ;x++){  //每一行的比较相同则 计算，是不同列 所以比较后都要退出换i值
                                     if(Number(this.panel[i].value) ==  Number(this.panel[x+1].value)){
-                                        This.add(i,x+1);
-                                        break;
-                                    }else{
-                                        break;
+                                        if(x-i+1 > 2 && Number(this.panel[i+1].value) == 0 && Number(this.panel[i+2].value) == 0){    //跨非空元素不能相加
+                                            This.add(i,x+1); //相邻的元素计算后推出
+                                            break;
+                                        }else if(x-i+1 > 1 && Number(this.panel[i+1].value) == 0){    //跨非空元素不能相加
+                                            This.add(i,x+1); //相邻的元素计算后推出
+                                            break; 
+                                        }else if(x-i+1 <= 1){
+                                            This.add(i,x+1); //相邻的元素计算后推出
+                                            break;
+                                        }
+                                        
                                     }
                                 }                   
                             }
@@ -177,12 +212,15 @@ new Vue({
             });
             if(arr.length == 0){
                 this.tipsShow = true;
+                this.record = this.score;
+                localStorage.setItem('score',this.record)
+
             }else if(this.touch.switch){    //如果有像加的计算
-                var newIndex = Math.round(Math.random()*arr.length);    //新元素的位置
+                var newIndex = Math.floor(Math.random()*arr.length);
+                newIndex = arr[newIndex];   //新元素的位置 
                 this.panel[newIndex].value = 2;
                 this.panel[newIndex].class = 'class2';
-            }
-            
+            }            
         },
         add:function(oldindex,newindex){
             var This = this;
@@ -191,6 +229,7 @@ new Vue({
             This.panel[oldindex].class = 'class';
             This.panel[newindex].class = 'class' + This.panel[newindex].value;
             this.touch.switch = true;
+            this.score += This.panel[newindex].value;
         },
         move:function(index1,index2){   //index1目标位置 index2被移动的位置
             this.panel[index1].value = this.panel[index2].value;
@@ -198,6 +237,33 @@ new Vue({
             this.panel[index2].value = '';
             this.panel[index2].class = 'class0';
             this.touch.switch = true;
+        },
+        reset:function(){
+            this.tipsShow = false;//关闭弹窗
+            this.panel = [];
+            for(var i = 0;i < 16;i++){
+                this.panel.push({value:'',class:'class'})
+            }
+            var set = [];
+            var length = set.length;
+            while(length < 3){
+                var key = Math.floor(Math.random()*16);
+                var x = true;
+                for(var i = 0;i < length;i++){
+                    if(key == set[i]){
+                        x = false;
+                    }
+                }
+                if(x){
+                    set.push(key);
+                    length = set.length;
+                }
+            }
+            for(var i = 0,len = set.length;i < len;i++){
+                this.panel[set[i]].value = 2;
+                this.panel[set[i]].class ='class2';
+            }
+        
         }
     }
 })
