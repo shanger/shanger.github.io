132faf05f0052282e84fb086f185d4c4647b96d3
Oamazing <2569202365@qq.com>
2016-07-29 18:55:05 +0800

    注释 调整

--- transtion/js/shoppingCart.js
+++ transtion/js/shoppingCart.js
@@ -1,59 +1,61 @@
+//A购物车的位置坐标（以按钮中心作为坐标原点)
 var sidebar = document.querySelector('.sidebar');
 var dot = document.querySelector('.dot');
 var img = document.querySelector('.sidebar > img');
-var By = dot.offsetTop;
-var Bx = dot.offsetLeft;
-var Ay = img.offsetTop;
-var Ax = document.querySelector('.sidebar').offsetLeft;	
+var By = 0;
+var Bx = 0;
+var Ay = 0;
+var Ax = 0;	
 changesize();
 document.querySelector('body').onresize = changesize;
 
 function changesize(){
+	console.log('gg');
 	sidebar.style.height = window.document.body.clientHeight;
 }
 var Switch = true;	//点击开关
 document.querySelector('input').addEventListener('click',function(){
-	//A购物车的位置坐标（以按钮中心作为坐标原点)
+	
 	if(Switch){
-		Switch = false;
-		var a = '',b = '';		
-		Ay = img.offsetTop;
+		Switch = false;		//动画开始关闭开关
+		var a = '',b = '';		//抛物线函数
+		speed = 10;				//动画速度
+		Ay = img.offsetTop;		//购物车图片的位置
 		Ax = document.querySelector('.sidebar').offsetLeft;	
-		By = dot.offsetTop;
+		By = dot.offsetTop;		//动画图片的位置
 		Bx = dot.offsetLeft;
-		console.log(Bx);
-		var Cy = By - Ay;	//第一个点
+		var Cy = By - Ay;	//第一个坐标点	
 		var Cx = Ax - Bx - document.querySelector('.dot').offsetWidth/2;
-		var Dy = Cy + 40;	//第二个点
+		var Dy = Cy + 40;	//第二个坐标点 自选的坐标点 + 40是想抛物线的效果
 		var Dx = Cx/2;
 		a = ( 2*Dy-Cy )/( 2*Math.pow(Dx,2) - Math.pow(Cx,2) );
 		b = ( Cy - Math.pow(Cx,2)*a)/Cx
-		dot.setAttribute('class','dot')
-		var x = 0;
+		dot.setAttribute('class','dot');
+		var x = 0;	
 		clearInterval(timer);
-		dot.style.left =  '50%';
-		dot.style.MarginLeft = "25px";	
-		dot.style.top =  By + 'px';
+		reset();
 		var timer = setInterval(function(){
 			if(x <  Cx){
 				dot.style.left = x + Bx + 'px';
 				dot.style.top =  By - (a*Math.pow(x,2) + b*x) + 'px';
-				x = x + 10;
+				x = x + speed;
 			}else{
-				Switch = true;
+				Switch = true;		//动画结束打开开关
 				clearInterval(timer);
-				dot.setAttribute('class','dot hidden');
-				img.setAttribute('class','dance');
+				dot.setAttribute('class','dot hidden');		//结束后隐藏dot
+				img.setAttribute('class','dance');	//img动画
 				//回到原来的位置
-				dot.style.left =  '50%';
-				dot.style.MarginLeft = "25px";	
-				dot.style.top =  By + 'px';
+				reset();
 				//购物车的小动画
 				setTimeout(function(){
-					img.setAttribute('class','');
+					img.setAttribute('class','');	//img动画结束
 				},300);
 			}		
 		},8);
 	}
-
-},false)
+},false);
+function reset(){
+	dot.style.left =  '50%';
+	dot.style.MarginLeft = "25px";	
+	dot.style.top =  By + 'px';
+}
