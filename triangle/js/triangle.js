89c72942516fab07988bdf189a12716bc9863d74
Oamazing <2569202365@qq.com>
2016-08-16 15:17:09 +0800

    ie上兼容有问题 ，setAttribute()

--- triangle/js/triangle.js
+++ triangle/js/triangle.js
@@ -2,17 +2,18 @@ var body = document.querySelector('body');
 var div = document.querySelector('body > div:nth-of-type(1)');
 var style = "background-color: #f60;color: white;width: 200px;height: 200px;margin:50px auto;border-radius: 50%;line-height: 200px;text-align: center;";
 body.addEventListener('mousemove',function(ev){
+
 	var x = ev.pageX;var y = ev.pageY;
 	var D = 0;var move = '';
-	if(y < 600){
-		if(x > (div.offsetLeft + div.offsetWidth/2)){
+	if(y < 600){	//600px内
+		if(x > (div.offsetLeft + div.offsetWidth/2)){	//右侧往右侧偏
 			D = Math.floor(3*y/10);	//180deg
-		}else{
+		}else{	//左侧往左侧偏
 			D = Math.floor(3*(600-y)/10) + 180;
 		}		
 		move = "transform: rotate("+D+"deg);-webkit-transform: rotate("+D+"deg);-ms-transform: rotate("+D+"deg);"
   			+"-moz-transform: rotate("+D+"deg);-o-transform: rotate("+D+"deg);";
-  		div.style = style + move;
+  		div.setAttribute('style',style + move);
 	}
 	
 },false)
