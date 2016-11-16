8141571a04a354e0dcf126247162a6c3d4916f07
Oamazing <2569202365@qq.com>
2016-11-16 14:29:39 +0800

    mongodb is comming & htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
 var gulp = require('gulp'),
+htmlmin = require('gulp-htmlmin'),
 minifycss = require('gulp-minify-css'),
 concat = require('gulp-concat'),
 uglify = require('gulp-uglify'),
 rename = require('gulp-rename'),
 del = require('del');
 
+
 //压缩
+gulp.task('html',function(){
+    var options = {
+        collapseWhitespace:true,    //清除空格
+        collapseBooleanAttributes:true, //省略布尔属性的值
+        removeComments:true,            //清除html中注释的部分
+        removeEmptyAttributes:true,     //清楚空属性
+        removeScriptTypeAttributes:true,//清除所有script标签中的type="text/javascript"属性
+        removeStyleLinkTypeAttributes:true,//清除所有Link标签中的type="text/stylesheet"属性
+        minifyJS:true,                      //压缩html中的js
+        minifyCSS:true                      //压缩html中的css
+    };
+    gulp.src('demo/vue-router/address.html')
+    .pipe(htmlmin(options))
+    .pipe(rename('addressMin.html'))
+    .pipe(gulp.dest('demo/vue-router')); 
+});
 gulp.task('minifycss', function() {
 	del(['demo/vue-router/compress/address.css']);
     return gulp.src('demo/vue-router/css/*.css')      //压缩的文件        
@@ -25,10 +43,10 @@ gulp.task('minifyjs', function() {
 
 //监控css
 gulp.task('watch', function () {
+    gulp.watch('demo/vue-router/address.html', ['html']);
     gulp.watch('demo/vue-router/css/*.css', ['minifycss']);
     gulp.watch('demo/vue-router/js/address.js', ['minifyjs']);
 });
 
 
-gulp.task('default',['minifycss','minifyjs','watch']);
-
+gulp.task('default',['html','minifycss','minifyjs','watch']);
--- nodeTest/spider/js/mongodb.js
+++ nodeTest/spider/js/mongodb.js
@@ -1,9 +1,9 @@
 //连接
 var MongoClient = require('mongodb').MongoClient;
-var DB_CONN_STR = 'mongodb://localhost:27017/wilsondbl';
+var DB_CONN_STR = 'mongodb://localhost:27017/shan';
 var insertData = function(db,callback){
 	//连接表
-	var collection = db.collection('tb2');
+	var collection = db.collection('tab1');
 	//插入数据
 	var data = [
 		{
@@ -12,7 +12,7 @@ var insertData = function(db,callback){
 			name:'zhang',age:23
 		}
 	];
-	collection.inster(data,function(err.result){
+	collection.insert(data,function(err,result){
 		if(err){
 			console.log('Error'+err);
 			return;
@@ -20,27 +20,9 @@ var insertData = function(db,callback){
 		callback(result);
 	})
 }
-
-MongoClient.connect(DB_CONN_STR,function(err,db){
-	console.log('连接成功');
-	insertData(db.function(result){
-		console.log(result);
-	});
-	selectData(db.function(result){
-		console.log(result);
-	});
-	updataDate(db.function(result){
-		console.log(result);
-	});
-	delDate(db.function(result){
-		console.log(result);
-		db.colse();
-	})
-});
-
 //查询
 var selectData = function(db,callback){
-	var collection = db.collection('tb2');
+	var collection = db.collection('tab1');
 	var whereStr = {name:'shan'};
 	collection.find(whereStr).toArray(function(err,result){
 		if(err){
@@ -54,7 +36,7 @@ var selectData = function(db,callback){
 //修改
 
 var updataDate = function(db,callback){
-	var collection = db.collection('tb2');
+	var collection = db.collection('tab1');
 	var whereStr = {name:'shan'};
 	var updateStr = {$set:{age:22}};
 	collection.update(whereStr,updateStr ,function(err,result){
@@ -68,7 +50,7 @@ var updataDate = function(db,callback){
 
 //删除
 var delDate = function(db,callback){
-	var collection = db.collection('tb2');
+	var collection = db.collection('tab1');
 	var whereStr = {name:'shan'};
 	collection.remove(whereStr,function(err,result){
 		if(err){
@@ -78,4 +60,23 @@ var delDate = function(db,callback){
 		callback(result);
 	})
 }
+
+MongoClient.connect(DB_CONN_STR,function(err,db){
+	console.log('连接成功');
+	insertData(db,function(result){
+		console.log(result);
+	});
+	selectData(db,function(result){
+		console.log(result);
+	});
+	updataDate(db,function(result){
+		console.log(result);
+	});
+	delDate(db,function(result){
+		console.log(result);
+		db.close();
+	})
+});
+
+
 
