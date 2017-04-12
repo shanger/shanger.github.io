5587110d29f4330dc26d0a220e0e65accaa2280e
Oamazing <2569202365@qq.com>
2017-04-12 10:59:44 +0800

    add src/flexible

--- demo/webpack/src/main.js
+++ demo/webpack/src/main.js
@@ -1,39 +1,3 @@
-// //es6语法：
-// // import Vue from "../node_modules/vue/dist/vue.min.js";//其实不用写完，会自动查找。
-// // 外部引入别的库都可以用这样的方式，比如jquery等。。
-// // 引入我们编写的测试用vue文件。
-
-// import MyApp from './components/radio.vue';
-// import picker from './components/citypicker.vue';
-// import autoComp from './components/autoComplet.vue';
-// import scrollTop2Load from './components/scrollTop2Load.vue';
-// import animateNav from './components/animateNav.vue';
-
-// Vue.config.debug = true;//开启错误提示
-
-// new Vue(MyApp);
-
-// new Vue(picker);
-
-// new Vue(autoComp);
-
-// new Vue(scrollTop2Load);
-// new Vue({   
-//     el: '#body',  
-//     components: { app }  
-// })  
-
-// var app = new Vue({
-// 	el:'#body',
-// 	data:{		
-// 		aa:'ggg'
-// 	}
-// 	data(){
-// 		return {
-// 			aa:'ggg'
-// 		}
-// 	}
-// })
 
 import Vue from "../node_modules/.2.1.6@vue/dist/vue.min.js";
 import VueRouter from "../node_modules/.2.3.0@vue-router/dist/vue-router.min.js"
