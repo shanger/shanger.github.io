9395b906b5595026bed36c960e8c736543a961c7
单国力 <shanguoli@changingedu.com>
2018-01-30 14:40:12 +0800

    modify

--- transtion/js/shoppingCart.js
+++ transtion/js/shoppingCart.js
@@ -5,56 +5,58 @@ var img = document.querySelector('.sidebar > img');
 var By = 0;
 var Bx = 0;
 var Ay = 0;
-var Ax = 0;	
+var Ax = 0;
 changesize();
 document.querySelector('body').onresize = changesize;
 
-function changesize(){
+function changesize() {
 	sidebar.style.height = window.document.body.clientHeight;
 }
-var Switch = true;	//点击开关
-document.querySelector('input').addEventListener('click',function(){
-	
-	if(Switch){
-		Switch = false;		//动画开始关闭开关
-		var a = '',b = '';		//抛物线函数
-		speed = 10;				//动画速度
-		Ay = img.offsetTop;		//购物车图片的位置
-		Ax = document.querySelector('.sidebar').offsetLeft;	
-		By = dot.offsetTop;		//动画图片的位置
+var Switch = true; //点击开关
+document.querySelector('input').addEventListener('click', function () {
+
+	if (Switch) {
+		Switch = false; //动画开始关闭开关
+		var a = '',
+			b = ''; //抛物线函数
+		speed = 10; //动画速度
+		Ay = img.offsetTop; //购物车图片的位置
+		Ax = sidebar.offsetLeft;
+		By = dot.offsetTop; //动画图片的位置
 		Bx = dot.offsetLeft;
-		var Cy = By - Ay;	//第一个坐标点	
-		var Cx = Ax - Bx - document.querySelector('.dot').offsetWidth/2;
-		var Dy = Cy + 40;	//第二个坐标点 自选的坐标点 + 40是想抛物线的效果
-		var Dx = Cx/2;
-		a = ( 2*Dy-Cy )/( 2*Math.pow(Dx,2) - Math.pow(Cx,2) );
-		b = ( Cy - Math.pow(Cx,2)*a)/Cx
-		dot.setAttribute('class','dot');
-		var x = 0;	
+		var Cy = By - Ay; //第一个坐标点	
+		var Cx = Ax - Bx - dot.offsetWidth / 2;
+		var Dy = Cy + 40; //第二个坐标点 自选的坐标点 + 40是想抛物线的效果
+		var Dx = Cx / 2;
+		a = (2 * Dy - Cy) / (2 * Math.pow(Dx, 2) - Math.pow(Cx, 2));
+		b = (Cy - Math.pow(Cx, 2) * a) / Cx
+		dot.setAttribute('class', 'dot');
+		var x = 0;
 		clearInterval(timer);
 		reset();
-		var timer = setInterval(function(){
-			if(x <  Cx){
+		var timer = setInterval(function () {
+			if (x < Cx) {
 				dot.style.left = x + Bx + 'px';
-				dot.style.top =  By - (a*Math.pow(x,2) + b*x) + 'px';
+				dot.style.top = By - (a * Math.pow(x, 2) + b * x) + 'px';
 				x = x + speed;
-			}else{
-				Switch = true;		//动画结束打开开关
+			} else {
+				Switch = true; //动画结束打开开关
 				clearInterval(timer);
-				dot.setAttribute('class','dot hidden');		//结束后隐藏dot
-				img.setAttribute('class','dance');	//img动画
+				dot.setAttribute('class', 'dot hidden'); //结束后隐藏dot
+				img.setAttribute('class', 'dance'); //img动画
 				//回到原来的位置
 				reset();
 				//购物车的小动画
-				setTimeout(function(){
-					img.setAttribute('class','');	//img动画结束
-				},300);
-			}		
-		},8);
+				setTimeout(function () {
+					img.setAttribute('class', ''); //img动画结束
+				}, 300);
+			}
+		}, 8);
 	}
-},false);
-function reset(){
-	dot.style.left =  '50%';
-	dot.style.MarginLeft = "25px";	
-	dot.style.top =  By + 'px';
+}, false);
+
+function reset() {
+	dot.style.left = '50%';
+	dot.style.MarginLeft = "25px";
+	dot.style.top = By + 'px';
 }
